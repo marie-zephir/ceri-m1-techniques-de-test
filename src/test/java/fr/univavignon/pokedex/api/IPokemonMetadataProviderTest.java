be60be2937f5c999c3b7729b7583f465bf52067a
Marie Zephir <mz@efidem.com>
2023-05-16 20:58:25 +0200

    -Refactor of the test IPokemonMetadataProviderTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -3,12 +3,17 @@ package fr.univavignon.pokedex.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class IPokemonMetadataProviderTest {
     PokemonMetadata example1;
     public IPokemonMetadataProvider iPokemonMetadataProviderExample1;
+
+    List<PokemonMetadata> iPokemonMetadata= new ArrayList<PokemonMetadata>();
     IPokedex iPokedexExample1;
     IPokemonFactory iPokemonFactoryExample1;
 
@@ -23,11 +28,13 @@ public class IPokemonMetadataProviderTest {
     @Test
     void testGetPokemonMetadata() throws PokedexException {
         assertEquals(PokemonMetadata.class, iPokedexExample1.getPokemonMetadata(example1.getIndex()).getClass());
-        assertEquals(0,example1.getIndex());
-        assertEquals("Bulbizarre",example1.getName());
-        assertEquals(126,example1.getAttack());
-        assertEquals(126,example1.getDefense());
-        assertEquals(90,example1.getStamina());
+        iPokemonMetadata.add(example1);
+        assertEquals(0,iPokemonMetadata.get(0).getIndex());
+        assertEquals("Bulbizarre",iPokemonMetadata.get(0).getName());
+        assertEquals(126,iPokemonMetadata.get(0).getAttack());
+        assertEquals(126,iPokemonMetadata.get(0).getDefense());
+        assertEquals(90,iPokemonMetadata.get(0).getStamina());
         assertThrows(PokedexException.class, () -> iPokedexExample1.getPokemonMetadata(-1));
+        assertThrows(PokedexException.class, () -> iPokedexExample1.getPokemonMetadata(-11));
     }
 }
