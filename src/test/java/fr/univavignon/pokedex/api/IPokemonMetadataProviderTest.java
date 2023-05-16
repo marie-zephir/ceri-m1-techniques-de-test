60481b34dc27b7111b0812e6ce5691e5abc72f8e
Marie Zephir <mz@efidem.com>
2023-05-16 20:06:47 +0200

    -Refactor of the test IPokemonMetadataProviderTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -4,22 +4,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class IPokemonMetadataProviderTest {
     PokemonMetadata example1;
-    PokemonMetadata example2;
     public IPokemonMetadataProvider iPokemonMetadataProviderExample1;
-    public IPokemonMetadataProvider iPokemonMetadataProviderExample2;
+    IPokedex iPokedexExample1;
+    IPokemonFactory iPokemonFactoryExample1;
+
     public  IPokemonMetadataProviderTest(){
         this.example1 = new PokemonMetadata(0,"Bulbizarre",126,126,90);
-        this.example2 = new PokemonMetadata(133,"Aquali",186,168,260);
-        this.iPokemonMetadataProviderExample1 = index -> example1;
-        this.iPokemonMetadataProviderExample2 = index -> example2;
+        iPokemonMetadataProviderExample1 = new PokemonMetadataProvider();
+        iPokemonFactoryExample1 = new PokemonFactory();
+        iPokedexExample1 = new Pokedex(iPokemonMetadataProviderExample1, iPokemonFactoryExample1);
+
     }
 
     @Test
     void testGetPokemonMetadata() throws PokedexException {
-        assertEquals(example1,iPokemonMetadataProviderExample1.getPokemonMetadata(0));
-        assertEquals(example2,iPokemonMetadataProviderExample2.getPokemonMetadata(133));
+        assertEquals(PokemonMetadata.class, iPokedexExample1.getPokemonMetadata(example1.getIndex()).getClass());
+        assertEquals(0,example1.getIndex());
+        assertEquals("Bulbizarre",example1.getName());
+        assertEquals(126,example1.getAttack());
+        assertEquals(126,example1.getDefense());
+        assertEquals(90,example1.getStamina());
+        assertThrows(PokedexException.class, () -> iPokedexExample1.getPokemonMetadata(-1));
     }
 }
